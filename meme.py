a2cd11053f242864a2856a1dd5c97558564ffa75
lamming0912@gmail.com <dev@dlops.ai>
2022-03-02 19:23:25 +0800

    correct code

--- meme.py
+++ meme.py
@@ -3,10 +3,10 @@ import requests, json, random
 f = open("./README.md", "w")
 res = requests.get(f'https://meme-api.herokuapp.com/gimme')
 result = json.loads(res.text)
-f.write(f'''<p align="center">
-    <img src="{result['url']}" >
-</p>
-<h3 align="center">{result['title]}</h3>
-<h3 align="center">Have a nice day!</h3>
-''')
+f.write(f"""<p align="center">
+        <img src="{result['url']}" >
+        </p>
+        <h3 align="center">{result['title']}</h3>
+        <h3 align="center">Have a nice day!</h3>
+    """)
 f.close()
