e21ed2839488f52e963385ab06b36c86caa86fb7
lamming0912@gmail.com <dev@dlops.ai>
2022-03-02 19:26:29 +0800

    changed image size

--- meme.py
+++ meme.py
@@ -4,9 +4,9 @@ f = open("./README.md", "w")
 res = requests.get(f'https://meme-api.herokuapp.com/gimme')
 result = json.loads(res.text)
 f.write(f"""<p align="center">
-        <img src="{result['url']}" >
+        <img src="{result['url']}" width="600" height="600">
         </p>
         <h3 align="center">{result['title']}</h3>
-        <h3 align="center">Have a nice day!</h3>
+        <h3 align="center">*Randomly generated meme every hour</h3>
     """)
 f.close()
